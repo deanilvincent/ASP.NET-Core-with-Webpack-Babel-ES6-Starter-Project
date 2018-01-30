0454d1a21a82374d2b620d0cd449948b363291aa
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-01-30 19:40:59 +0800

    Add project files.

---
+++ ASPNETCoreWithWBES6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCoreWithWBES6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
