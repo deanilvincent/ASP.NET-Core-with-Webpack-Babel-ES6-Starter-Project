44bfd5de31d77091360d5e74fb34fccf3e4b3a33
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-02-08 23:55:07 +0800

    commit js and webpack

---
+++ ASPNETCoreWithWBES6/webpack.config.js
@@ -0,0 +1,31 @@
+﻿var path = require("path");
+
+module.exports = [
+    {
+        entry: [
+            "babel-polyfill",
+            "./Scripts/app.js"
+        ],
+        output: {
+            publicPath: "/js/",
+            path: path.join(__dirname, "/wwwroot/js/"),
+            filename: "app.build.js"
+        },
+        module: {
+            loaders: [
+                {
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    query: {
+                        presets: ["es2015", "stage-1"]
+                    }
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                vue: "vue/dist/vue.js"
+            }
+        }
+    }
+];
